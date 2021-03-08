0ca06470a4ca0edb282d60caaac760aa5458a117
codemobiles <chaiyasit.t@gmail.com>
2021-03-08 11:39:05 +0700

    1234

--- demo1/App.js
+++ demo1/App.js
@@ -7,15 +7,15 @@ export default function App() {
       style={{
         backgroundColor: '#FF0',
         flex: 1,
-        alignItems: 'flex-end',
-        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        justifyContent: 'space-evenly',
         flexDirection: 'column',
       }}>
-      <Text style={{fontSize: 40, backgroundColor: 'white'}}>1</Text>
-      <Text style={{fontSize: 40, backgroundColor: 'red'}}>2</Text>
-      <Text style={{fontSize: 40, backgroundColor: 'purple'}}>3</Text>
-      <Text style={{fontSize: 40, backgroundColor: 'pink'}}>4</Text>
-      <Text style={{fontSize: 40, backgroundColor: 'green'}}>5</Text>
+      <Text style={{fontSize: 40, backgroundColor: 'white', flex: 1}}>1</Text>
+      <Text style={{fontSize: 40, backgroundColor: 'red', flex: 1}}>2</Text>
+      <Text style={{fontSize: 40, backgroundColor: 'purple', flex: 1}}>3</Text>
+      <Text style={{fontSize: 40, backgroundColor: 'pink', flex: 1}}>4</Text>
+      <Text style={{fontSize: 40, backgroundColor: 'green', flex: 1}}>5</Text>
     </View>
   );
 }
