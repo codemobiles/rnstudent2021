c190dd2760d98f3c3ece83cfefeace39483c0006
codemobiles <chaiyasit.t@gmail.com>
2021-03-08 13:44:25 +0700

    1234

--- demo2/src/HomeScreen.js
+++ demo2/src/HomeScreen.js
@@ -21,11 +21,29 @@ export default function HomeScreen() {
       <View
         style={{
           height: 100,
-          backgroundColor: '#FFF',
+          backgroundColor: '#FFF7',
           marginLeft: 30,
           marginRight: 30,
+          borderRadius: 10,
+          padding: 15,
         }}>
-        <TextInput />
+        {/* Username */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{height: 35, width: 35, backgroundColor: 'green'}} />
+          <TextInput
+            placeholder="Username"
+            style={{backgroundColor: '#0003', flex: 1, marginLeft: 5}}
+          />
+        </View>
+
+        {/* Password */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{height: 35, width: 35, backgroundColor: 'red'}} />
+          <TextInput
+            placeholder="Password"
+            style={{backgroundColor: '#0003', flex: 1, marginLeft: 5}}
+          />
+        </View>
       </View>
     </ImageBackground>
   );
