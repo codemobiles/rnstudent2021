c11d7cc54cad37df1c4424f523c19521670f2d86
codemobiles <chaiyasit.t@gmail.com>
2021-03-08 11:13:54 +0700

    1234

--- demo1/App.js
+++ demo1/App.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={{backgroundColor: '#FF0'}}>
       <Text>Hey</Text>
     </View>
   );
