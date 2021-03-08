404ba71f977e9e3f0763e17047a775488b79297d
codemobiles <chaiyasit.t@gmail.com>
2021-03-08 14:44:34 +0700

    1234

---
+++ demo2/src/RegisterScreen.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Platform,
+  Button,
+} from 'react-native';
+
+export default function HomeScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  return (
+    <ImageBackground source={require('./assets/img/bg.png')} style={{flex: 1}}>
+      <Image
+        resizeMode="contain"
+        style={{width: '100%'}}
+        source={require('./assets/img/header_react_native.png')}
+      />
+      <View
+        style={{
+          backgroundColor: '#FFF7',
+          marginLeft: 30,
+          marginRight: 30,
+          borderRadius: 10,
+          padding: 15,
+        }}>
+        {/* Username */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{height: 35, width: 35, backgroundColor: 'green'}} />
+          <TextInput
+            placeholder="Username"
+            onChangeText={(text) => setUsername(text)}
+            style={{backgroundColor: '#0003', flex: 1, marginLeft: 5}}
+          />
+        </View>
+
+        {/* Separator */}
+        <View style={{height: 1, backgroundColor: '#fff3', width: null}} />
+
+        {/* Password */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{height: 35, width: 35, backgroundColor: 'red'}} />
+          <TextInput
+            onChangeText={(text) => setPassword(text)}
+            placeholder="Password"
+            style={{backgroundColor: '#0003', flex: 1, marginLeft: 5}}
+          />
+        </View>
+
+        {/* Register Button */}
+        <View style={{height: 10}} />
+        <Button title="Register" onPress={() => alert(username + password)} />
+
+        {/* Cancel Button */}
+        <View style={{height: 10}} />
+        <TouchableOpacity
+          style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Text>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+}
