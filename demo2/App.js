05023a3a5f78cfabfe3642f89764ee3311fda98d
codemobiles <chaiyasit.t@gmail.com>
2021-03-08 13:10:33 +0700

    1234

--- demo2/App.js
+++ demo2/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import HomeScreen from './src/HomeScreen';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hey</Text>
-    </View>
-  );
+  return <HomeScreen />;
 }
