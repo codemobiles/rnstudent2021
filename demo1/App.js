01922436f4a5ba263e132c0d3ba9ad154a3650e6
codemobiles <chaiyasit.t@gmail.com>
2021-03-08 11:09:27 +0700

    1234

--- demo1/App.js
+++ demo1/App.js
@@ -3,22 +3,8 @@ import {View, Text} from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: 'red',
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          backgroundColor: 'yellow',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign:'center'
-        }}>
-        555
-      </Text>
+    <View>
+      <Text>Hey</Text>
     </View>
   );
 }
