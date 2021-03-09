10a3614a66ed34dc19aa4fb0082b778071017995
codemobiles <chaiyasit.t@gmail.com>
2021-03-09 13:15:09 +0700

    1234

--- demo2/src/JSONFeedScreen.js
+++ demo2/src/JSONFeedScreen.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import axios from 'axios';
 
 export default function JSONFeedScreen() {
   const [dataArray, setDataArray] = React.useState([]);
@@ -25,9 +34,45 @@ export default function JSONFeedScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
   },
-  body: {
-    color: 'white',
+  list_header: {
+    width: '100%',
+    height: 100,
+  },
+  listCard: {
+    overflow: 'hidden',
+    flexDirection: 'column',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 0,
+  },
+  listCardView: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    height: 45,
+    alignItems: 'center',
+  },
+  listAvatar: {
+    width: 45,
+    height: '100%',
+    marginRight: 16,
+  },
+  listTitleSubtitleContainer: {
+    flexDirection: 'column',
+    marginRight: 16,
+    flex: 1,
+  },
+  listTitle: {
+    fontWeight: '700',
+  },
+  listSubTitle: {
+    fontWeight: '100',
+  },
+  listYoutbeImage: {
+    width: '100%',
+    height: 190,
   },
 });
