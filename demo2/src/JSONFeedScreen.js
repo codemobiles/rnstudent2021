43685564ca3cbac111291438d4e06da7b362d141
codemobiles <chaiyasit.t@gmail.com>
2021-03-09 11:52:06 +0700

    1234

--- demo2/src/JSONFeedScreen.js
+++ demo2/src/JSONFeedScreen.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 export default function JSONFeedScreen() {
+  const [dataArray, setDataArray] = React.useState([]);
+
   React.useEffect(() => {
     console.log('Hey');
+    setDataArray([1, 2, 3, 4, 5, 6, 7, 8]);
   }, []);
 
   return (
     <View style={{flex: 1}}>
-      {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+      {dataArray.map((item) => (
         <Text key={item.toString()}>JSONFeedScreen {item}</Text>
       ))}
     </View>
