7412797b8f84edcce34cc35001f6606e5f41c6f0
codemobiles <chaiyasit.t@gmail.com>
2021-03-09 12:06:06 +0700

    1234

--- demo2/src/JSONFeedScreen.js
+++ demo2/src/JSONFeedScreen.js
@@ -6,7 +6,7 @@ export default function JSONFeedScreen() {
 
   React.useEffect(() => {
     console.log('Hey');
-    setDataArray([1, 2, 3, 4, 5, 6, 7, 8]);
+    setDataArray([1, 2, 3, 4, 5, 6, 7, 8]);   
   }, []);
 
   return (
