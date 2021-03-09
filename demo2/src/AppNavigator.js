f8781ae5b4ded2e338a842ccfda95bf1fa630227
codemobiles <chaiyasit.t@gmail.com>
2021-03-09 09:35:34 +0700

    1234

---
+++ demo2/src/AppNavigator.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Image} from 'react-native';
+
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './HomeScreen';
+import RegisterScreen from './RegisterScreen';
+import JSONFeedScreen from './JSONFeedScreen';
+import CameraScreen from './CameraScreen';
+import YoutubeScreen from './YoutubeScreen';
---
+++ demo2/src/CameraScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function CameraScreen() {
+  return (
+    <View>
+      <Text>CameraScreen</Text>
+    </View>
+  );
+}
