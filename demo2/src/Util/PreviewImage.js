386393f9e9748284821f77b60aff492d72d81e50
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 09:39:59 +0700

    1234

--- demo2/src/Util/PreviewImage.js
+++ demo2/src/Util/PreviewImage.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, View} from 'react-native';
 
 export const PreviewImage = ({image}) => {
   if (image == null) {
     return (
-      <Image
-        source={require('../assets/img/cmdev_icon.png')}
-        style={{flex: 1, width: '100%'}}
-      />
+      <View style={{padding: 70, flex: 1}}>
+        <Image
+          source={require('../assets/img/cmdev_icon.png')}
+          style={{flex: 1, width: '100%'}}
+          resizeMode="contain"
+        />
+      </View>
     );
   }
 };
