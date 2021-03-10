b220b3bada81af8c33fcd2f5f897ddb4c951c56b
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 14:56:36 +0700

    1234

--- demo3/src/TabQRcode.js
+++ demo3/src/TabQRcode.js
@@ -54,7 +54,6 @@ export default function TabQRcode() {
           alignItems: 'center',
         }}>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          
           <QRCode key={item.toString()} value="http://awesome.link.qr" />
         ))}
       </ScrollView>
