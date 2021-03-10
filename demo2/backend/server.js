cc32e1e80a96230fb8a29a6948c6fddd498b2c50
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 10:49:32 +0700

    1234

--- demo2/backend/server.js
+++ demo2/backend/server.js
@@ -21,7 +21,21 @@ app.get('/register/:username/:password', (req, res) => {
 app.post('/uploads', (req, res) => {
   let form = new formidable.IncomingForm();
   form.parse(req, (error, fields, files) => {
-    res.json({fields, files});
+    // res.json({fields, files});
+
+    var newname = Date.now();
+      var oldpath = files.userfile.path;
+      var newpath =
+        __dirname +
+        '/upload/' +
+        newname.toString() +
+        '.' +
+        files.userfile.name.split('.').pop();
+  
+      fs.move(oldpath, newpath, function (err) {
+        res.json({result: 'Upload Successfully', account: fields});
+      });
+      
   });
 });
 
