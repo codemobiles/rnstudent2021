7c90aea994e539bc3b01577e7fc2664a47324f2f
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 10:48:25 +0700

    1234

--- demo2/backend/server.js
+++ demo2/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const formidable = require("formidable")
+const formidable = require('formidable');
 const app = express();
+const fs = require('fs-extra');
+
 
 app.get('/', (req, res) => {
   res.json({result: 'ok'});
@@ -16,13 +18,12 @@ app.get('/register/:username/:password', (req, res) => {
   res.json({result: 'success', detail: req.params});
 });
 
-
 app.post('/uploads', (req, res) => {
-    let form = new formidable.IncomingForm();
-    form.parse(req, (error, fields, files) => {
-      res.json({error, fields, files})
-    });
+  let form = new formidable.IncomingForm();
+  form.parse(req, (error, fields, files) => {
+    res.json({fields, files});
   });
+});
 
 app.listen(3000, () => {
   console.log('Server is running.');
