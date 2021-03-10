585c0d3317042b429bef769ff02ddc4680908e22
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 10:15:30 +0700

    1234

--- demo2/backend/server.js
+++ demo2/backend/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.json({result: 'ok'});
-// });
+app.get('/', (req, res) => {
+  res.json({result: 'ok'});
+});
 
-// app.get('/login', (req, res) => {
-//     res.json({result: 'success', detail: req.query});
-//   });
+// http://localhost:3000/login?username=admin&password=1234
+app.get('/login', (req, res) => {
+  res.json({result: 'success', detail: req.query});
+});
+
+// http://localhost:3000/register/lek/1234
+app.get('/register/:username/:password', (req, res) => {
+  res.json({result: 'success', detail: req.params});
+});
 
 app.listen(3000, () => {
   console.log('Server is running.');
