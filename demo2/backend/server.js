2c5a4f5e1b65b444ddc76a6014a05b28f125bdb2
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 10:07:06 +0700

    1234

--- demo2/backend/server.js
+++ demo2/backend/server.js
@@ -1,1 +1,14 @@
-console.log('Lek');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({result: 'ok'});
+});
+
+app.get('/login', (req, res) => {
+    res.json({result: 'success', detail: req.query});
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running.');
+});
