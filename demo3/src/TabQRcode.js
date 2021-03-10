af5d9fc0182bdf996dfbf7863d8d5c1cd11d3020
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 15:10:31 +0700

    1234

--- demo3/src/TabQRcode.js
+++ demo3/src/TabQRcode.js
@@ -54,7 +54,7 @@ export default function TabQRcode() {
           alignItems: 'center',
         }}>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          <CMQRCode />
+          <CMQRCode key={item.toString()} value={qrValue} />
         ))}
       </ScrollView>
     </ImageBackground>
