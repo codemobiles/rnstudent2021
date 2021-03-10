b57702adf3a5271b138602e528a621df07f99279
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 14:33:25 +0700

    1234

--- demo3/App.js
+++ demo3/App.js
@@ -12,22 +12,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// import React from 'react';
-// import {View, SafeAreaView, StatusBar, LogBox} from 'react-native';
-// import {NavigationContainer} from '@react-navigation/native';
-// import AppNavigator from "./src/AppNavigator";
-
-// export default function App() {
-
-//   LogBox.ignoreAllLogs(true)
-//   //console.disableYellowBox = true
-
-//   return (
-//     <NavigationContainer>
-//       <View style={{flex: 1}}>
-//         <AppNavigator/>
-//       </View>
-//     </NavigationContainer>
-//   );
-// }
--- demo3/src/AppNavigator.js
+++ demo3/src/AppNavigator.js
@@ -5,3 +5,17 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TabQRcode from './TabQRcode';
 import ScannerScreen from './ScannerScreen';
 import TabScanner from './TabScanner';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabScreens = () => {
+  return (
+    <Tab.Navigator initialRouteName="tab1">
+      <Tab.Screen name="tab1" component={TabQRcode} />
+      <Tab.Screen name="tab2" component={TabScanner} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabScreens;
