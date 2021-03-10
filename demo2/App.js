48d85e0fd427541cbc5efd2930a0814f2dcf36d9
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 12:04:22 +0700

    1234

--- demo2/App.js
+++ demo2/App.js
@@ -1,21 +1,33 @@
 import React, {useState, useEffect} from 'react';
 
-
+import AsyncStorage from '@react-native-community/async-storage';
 import {View, SafeAreaView, StatusBar} from 'react-native';
 import AppNavigator from './src/AppNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
-
-  const [forceLogin, setForceLogin] = useState(false);
+  const [forceLogin, setForceLogin] = useState(true);
   const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    checkAuthorization();
+  }, [isReady]);
+
+  const checkAuthorization = async () => {
+    let isAuthorized = await AsyncStorage.getItem('already_logged_in');
+    if (isAuthorized && isAuthorized == 'yes') {
+      setForceLogin(false);
+    }
+
+    setIsReady(true);
+  };
 
   return (
     <NavigationContainer>
       <View style={{flex: 1}}>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView />
-        <AppNavigator forceLogin={false} />
+        {isReady && <AppNavigator forceLogin={forceLogin} />}
       </View>
     </NavigationContainer>
   );
