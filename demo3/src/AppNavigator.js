f971bbb79ab279c2315dd14a17449b1631eb5c41
codemobiles <chaiyasit.t@gmail.com>
2021-03-10 14:39:00 +0700

    1234

--- demo3/src/AppNavigator.js
+++ demo3/src/AppNavigator.js
@@ -9,22 +9,58 @@ import TabScanner from './TabScanner';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const tab1_Option = {
+  tabBarLabel: 'QRcode',
+  tabBarIcon: ({focused}) => (
+    <Image
+      style={{
+        height: 28,
+        width: 28,
+      }}
+      resizeMode="contain"
+      source={
+        focused
+          ? require('./img/ic_qr_code_press.png')
+          : require('./img/ic_qr_code_normal.png')
+      }
+    />
+  ),
+};
+
+const tab2_Option = {
+  tabBarLabel: 'Scanner',
+  tabBarIcon: ({focused}) => (
+    <Image
+      style={{
+        height: 28,
+        width: 28,
+      }}
+      resizeMode="contain"
+      source={
+        focused
+          ? require('./img/ic_qr_scan_press.png')
+          : require('./img/ic_qr_scan_normal.png')
+      }
+    />
+  ),
+};
+
 const TabScreens = () => {
   return (
     <Tab.Navigator initialRouteName="tab1">
-      <Tab.Screen name="tab1" component={TabQRcode} />
-      <Tab.Screen name="tab2" component={TabScanner} />
+      <Tab.Screen name="tab1" component={TabQRcode} options={tab1_Option} />
+      <Tab.Screen name="tab2" component={TabScanner} options={tab2_Option} />
     </Tab.Navigator>
   );
 };
 
-const RootStack = ()=>{
+const RootStack = () => {
   return (
-    <Stack.Navigator initialRouteName="tab">
-      <Stack.Screen name="tab" component={TabScreens}/>
-      <Stack.Screen name="scan" component={ScannerScreen}/>
+    <Stack.Navigator initialRouteName="">
+      <Stack.Screen name="tab" component={TabScreens} />
+      <Stack.Screen name="scan" component={ScannerScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default RootStack;
