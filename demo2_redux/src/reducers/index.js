49fc847f322fbfea605984e8a26e428f414e2fb8
codemobiles <chaiyasit.t@gmail.com>
2021-03-11 11:35:34 +0700

    1234

---
+++ demo2_redux/src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+import jsonReducer from './jsonscreen.reducer';
+
+export default combineReducers({
+  jsonReducer,
+});
--- demo2_redux/src/reducers/jsonscreen.reducer.js
+++ demo2_redux/src/reducers/jsonscreen.reducer.js
@@ -1,16 +1,31 @@
+import {JSON_FAILED, JSON_FETCHING, JSON_SUCCESS} from '../Constants';
+
 const initialState = {
-    dataArray: [],
-    isRefreshing: false,
-    isFailed: false,
-}
+  dataArray: [],
+  isRefreshing: false,
+  isFailed: false,
+};
 
-export default (state = initialState, { type, payload }) => {
-    switch (type) {
-
-    case typeName:
-        return { ...state, ...payload }
+export default (state = initialState, {type, payload}) => {
+  switch (type) {
+    case JSON_FETCHING:
+      return {...state, isRefreshing: true, isFailed: false, dataArray: []};
+    case JSON_SUCCESS:
+      return {
+        ...state,
+        isRefreshing: false,
+        isFailed: false,
+        dataArray: payload,
+      };
+    case JSON_FAILED:
+      return {
+        ...state,
+        isRefreshing: false,
+        isFailed: true,
+        dataArray: payload,
+      };
 
     default:
-        return state
-    }
-}
+      return state;
+  }
+};
