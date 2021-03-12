32e92c5617d6b449aa2ff6990dfe703988606e47
codemobiles <chaiyasit.t@gmail.com>
2021-03-12 14:36:00 +0700

    1234

--- demo3_redux/index.js
+++ demo3_redux/index.js
@@ -2,8 +2,27 @@
  * @format
  */
 
+ import React from 'react';
+
+// Redux Begin
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducers from './src/reducers';
+// Redux End
+
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+const store = createStore(reducers, applyMiddleware(logger, thunk));
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
